cb4ef89c798a190b2b93d18b320eb39acd85fc38
Aleksandar <aleksandar.dmitrovic@gmail.com>
2021-02-01 08:54:49 -0700

    Comment out redux boilerplate temp

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './actions';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { increment, decrement } from './actions';
 import Board from './components/Board';
 
 function App() {
-  const counter = useSelector(state => state.counter)
-  const isLogged = useSelector(state => state.isLogged);
-  const dispatch = useDispatch();
+  // const counter = useSelector(state => state.counter)
+  // const isLogged = useSelector(state => state.isLogged);
+  // const dispatch = useDispatch();
 
   return (
     <div className="App">
-      <h1>Counter {counter}</h1>
+      <h1>Minesweeper</h1>
+      {/* <p>{counter}</p>
       <button onClick={() => dispatch(increment(5))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Valuable Info I shouldn't see</h3> : 'not logged in'} */}
       <Board />
 
-      {isLogged ? <h3>Valuable Info I shouldn't see</h3> : 'not logged in'}
     </div>
   );
 }
